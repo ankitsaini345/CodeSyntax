a6287c75d10d4f7c3880524d4801987aa1b7e478
Ankit Saini <ankitsaini345@gmail.com>
2021-04-05 12:30:44 +0530

    set map iterator

---
+++ javascript/Set, Map, Iterators, symbol/Symbol.js
@@ -0,0 +1,28 @@
+const s1 = Symbol();
+const s2 = Symbol.iterator;
+const s3 = Symbol(123);
+const s4 = Symbol('Hello');
+// console.log(typeof s1)
+// console.log(typeof s2)
+// console.log(typeof s3)
+// console.log(typeof s4)
+console.log(Symbol(123) == Symbol(123))
+const sym1 = Symbol.for(555);
+console.log(Symbol.keyFor(sym1))
+console.log(Symbol.for(555) === Symbol.for(555))
+
+//
+let obj = {}
+obj.prop1 = 'value1';
+// console.log(obj);
+const sym2 = Symbol('prop2');
+const sym3 = Symbol('value3')
+obj[sym2] = 'value2'
+obj.prop3 = sym3;
+for (const i in obj) {
+
+    console.log(i, obj[i]);
+
+
+}
+console.log(JSON.stringify(obj))
